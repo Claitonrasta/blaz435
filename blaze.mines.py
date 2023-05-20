89d4973bfdc92857a9b1d78dfa07dbdf1c14920c
Claitonrasta <73598418+Claitonrasta@users.noreply.github.com>
2023-05-20 14:47:26 -0300

    blaze.mines.py

--- blaze.mines.py
+++ blaze.mines.py
@@ -1,72 +1,121 @@
 import random
 import re
 import telebot
+import threading
 import time
+import sqlite3
 
-# Criar uma instância do bot usando o token de acesso
-bot = telebot.TeleBot('6052245829:AAFbs55v_Y0-C2XvloyaprHjpOJhQ0orzs8')
+bot = telebot.TeleBot('6142935832:AAFVueVryNIQtwWUa6KczAqqEhSeFc4P1I0')
 
-# Deletar o webhook ativo
 bot.delete_webhook()
 
-# Dicionário para armazenar o último horário de envio de mensagem por cada usuário
-last_message_time = {}
+active_codes = set()
+user_last_interaction = {}
 
-# Definir um manipulador de mensagens para o comando /minesblaze
+# Define uma função que remove o código da lista de códigos ativos
+def remove_code(code):
+    active_codes.remove(code)
+
+# Define uma função que verifica se o tempo de espera para o código expirou
+def check_code_timeout(code, timeout=600):
+    time.sleep(timeout)
+    remove_code(code)
+
+# Define uma função que cria uma nova conexão com o banco de dados para cada thread
+def get_conn():
+    return sqlite3.connect('minesblaze.db')
+
+# Lista de IDs permitidos
+allowed_ids = [5754319114, 987654321]
+
+# Tratador de mensagem para o comando /minesblaze
 @bot.message_handler(commands=['minesblaze'])
 def jogar(message):
+    global active_codes
+
+    # Verifica se o ID do chat está na lista de IDs permitidos
+    if message.chat.id not in allowed_ids:
+        bot.send_message(message.chat.id, "*Você não tem permissão para usar este comando.*", parse_mode="Markdown")
+        return
+
+    user_id = message.from_user.id
+    current_time = time.time()
+    last_interaction_time = user_last_interaction.get(user_id)
+
+    if last_interaction_time and (current_time - last_interaction_time) < 600:
+        remaining_time = int(600 - (current_time - last_interaction_time))
+        bot.send_message(message.chat.id, f"*Aguarde mais {remaining_time} segundos antes de jogar novamente.*", parse_mode="Markdown")
+        return
+
     code = message.text.split()[1] if len(message.text.split()) > 1 else None
-    
-    if code and len(code) >= 10 and re.match("^[a-zA-Z0-9]*$", code):
-        user_id = message.from_user.id
-        current_time = time.time()
-        
-        if user_id in last_message_time and current_time - last_message_time[user_id] < 600:
-            bot.send_message(message.chat.id, "Você só pode enviar uma mensagem a cada 10 minutos.")
-        else:
-            last_message_time[user_id] = current_time
-            
-            # Representação do tabuleiro
-            board = [
-                ["🟥", "🟥", "🟥", "🟥", "🟥"],
-                ["🟥", "🟥", "🟥", "🟥", "🟥"],
-                ["🟥", "🟥", "🟥", "🟥", "🟥"],
-                ["🟥", "🟥", "🟥", "🟥", "🟥"],
-                ["🟥", "🟥", "🟥", "🟥", "🟥"]
-            ]
 
-            # Sortear 3 casas aleatórias
-            selected_cells = random.sample(range(25), 3)
+    if code and len(code) == 33 and re.match("^[a-zA-Z0-9]*$", code):
+        if code in active_codes:
+            bot.send_message(message.chat.id, "*Por favor, aguarde 10 minutos antes de jogar novamente.*", parse_mode="Markdown")
+            return
 
-            # Marcar as casas sorteadas no tabuleiro
-            for cell in selected_cells:
-                row = cell // 5
-                col = cell % 5
-                board[row][col] = "✅"
+        # Adiciona o código à lista de códigos ativos
+        active_codes.add(code)
 
-            # Criar uma string com a representação do tabuleiro com as casas selecionadas marcadas
-            text = "┌─────────────────────┐\n"
-            for row in range(5):
-                text += "│"
-                for col in range(5):
-                    if board[row][col] == "🟥":
-                        text += "🟥"
-                    else:
-                        text += " ✅ │"
-                text += "\n"
-                if row != 4:
-                    text += "├─────────────────────┤\n"
-            text += "└─────────────────────┘"
+        # Cria um novo thread para verificar o tempo de espera do código
+        threading.Thread(target=check_code_timeout, args=(code,), daemon=True).start()
 
-            # Adicionar porcentagem aleatória entre 60% e 94%
-            percent = round(random.uniform(60, 94), 2)
-            text += f"\n\nA TAXA DE ACERTO DESSA DESCRIPTOGRAFIA É {percent}%, COM O MÁXIMO DE 2 GALES!!!"
+        # Atualiza o horário da última interação do usuário
+        user_last_interaction[user_id] = current_time
 
-            # Enviar a mensagem com o tabuleiro e a porcentagem para o chat que iniciou o bot
-            bot.send_message(message.chat.id, text)
+        # Cria uma nova conexão com o banco de dados para essa thread
+        with get_conn() as conn:
+            c = conn.cursor()
+
+            # Verifica se o código já foi enviado antes
+            c.execute("SELECT message FROM messages WHERE code=?", (code,))
+            result = c.fetchone()
+
+            if result:
+                # Se o código já foi enviado, envia a mensagem armazenada no banco de dados
+                bot.send_message(message.chat.id, result[0], parse_mode="Markdown")
+            else:
+                # Se o código ainda não foi enviado, gera uma nova mensagem
+                welcome_message = "*Bem-vindo Ao DescryptoBlaze!*"
+
+                board = [
+                    ["💣", "💣", "💣", "💣", "💣"],
+                    ["💣", "💣", "💣", "💣", "💣"],
+                    ["💣", "💣", "💣", "💣", "💣"],
+                    ["💣", "💣", "💣", "💣", "💣"],
+                    ["💣", "💣", "💣", "💣", "💣"]
+                ]
+
+                selected_cells = random.sample(range(25), 4)
+
+                for cell in selected_cells:
+                    row = cell // 5
+                    col = cell % 5
+                    board[row][col] = "💎"
+
+                text = f"{welcome_message}\n\n"
+                for row in range(5):
+                    text += "|"
+                    for col in range(5):
+                        if board[row][col] == "💣":
+                            text += "💣|"
+                        else:
+                            text += "💎|"
+                    text += "\n"
+                    if row != 4:
+                        text += "                   \n"
+                text += "                   "
+
+                percent = round(random.uniform(60, 94), 2)
+                text += f"\n\n*A TAXA DE ACERTO DESSA DESCRIPTOGRAFIA É *{percent}*, COM O MÁXIMO DE 2 GALES!!!*"
+
+                # Armazena a mensagem no banco de dados
+                c.execute("INSERT INTO messages (code, message) VALUES (?, ?)", (code, text))
+                conn.commit()
+
+                # Envia a mensagem para o usuário
+                bot.send_message(message.chat.id, text, parse_mode="Markdown")
     else:
-        bot.send_message(message.chat.id, "ERRO AO TENTAR DESCRIPTOGRAFAR O PADRÃO DESSE TOKEN")
+        bot.send_message(message.chat.id, "*ID INVALIDO!! VERIFIQUE SE COPIOU O CÓDIGO CERTO, OBSERVE SE O ID TEM 33 CARACTERS, E SE O MESMO POSSUI SOMENTE NÚMEROS E LETRAS.*", parse_mode="Markdown")
 
-# Iniciar o bot
 bot.polling()
-
